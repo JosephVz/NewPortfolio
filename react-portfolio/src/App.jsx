2264655812a8962bfa40ad9105243cccfcbe0a95
unknown <vanzandt.school.email@gmail.com>
2024-02-22 16:32:10 -0500

    Working website to fill in with info.

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,10 +1,16 @@
 import styles from './App.module.css';
-import { Navbar } from "./components/Navbar/Navbar"
+import { Navbar } from "./components/Navbar/Navbar";
+import { Hero } from "./components/Hero/Hero";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
 
 function App() {
   return (
   <div className={styles.App}>
     <Navbar />
+    <Hero />
+    <About />
+    <Contact />
   </div>
   );
 }
