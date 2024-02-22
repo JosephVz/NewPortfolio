31c4ebc1db20b387d3b17f27b78436a65b3ef642
unknown <vanzandt.school.email@gmail.com>
2024-02-22 17:03:58 -0500

    Updated projects sections.

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -16,28 +16,9 @@ export const About = () => {
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>Student</h3>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
-              <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
-              <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+                I am currently a student enrolled in UNCC Charlotte coding bootcamp.
               </p>
             </div>
           </li>
--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -6,34 +6,35 @@ import { getImageUrl } from "../../utils";
 export const Hero = () => {
   return (
     <section className={styles.container} id="about">
-      <h2 className={styles.title}>About</h2>
+      <h2 className={styles.title}>Projects</h2>
       <div className={styles.content}>
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>Project 1</h3>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
+                This is the first project i had worked on in this bootcamp.
+                The project consists of a movie api which searches for your movie title and tells you details about it as well as a picture.
               </p>
+              <img
+          src={getImageUrl("projects/project.png")}
+          alt="Me"
+          className={styles.aboutImage}
+        />
             </div>
           </li>
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
+              <h3>Project 2</h3>
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+                This is the second project i had worked on in this bootcamp.
+                The project of a food app which has a functioning login and logout section which then logs you into a food api which searches up recipes.
               </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
-              <p>
-                I have designed multiple landing pages and have created design
-                systems as well
-              </p>
+              <img
+          src={getImageUrl("projects/Project2.png")}
+          alt="Me"
+          className={styles.aboutImage}
+        />
             </div>
           </li>
         </ul>
